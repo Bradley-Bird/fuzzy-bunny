4b7f86caede8ea4192050e4bf2de245d4f680dc3
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-21 14:10:22 -0800

    updated render function per intructions(families.js 19)

---
+++ render-utils.js
@@ -0,0 +1,20 @@
+export function renderFamily(family) {
+    const div = document.createElement('div');
+    const h3 = document.createElement('h3');
+    div.classList.add('family');
+    // h3.classList.add('family-name');
+    h3.textContent = family.name;
+
+    div.append(h3);
+    return div;
+}
+
+export function renderBunny(bunny) {
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+    div.classList.add('bunnies')
+    p.classList.add('bunny');
+    p.textContent = bunny.name;
+    div.append(p);
+    return div;
+}
